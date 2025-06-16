7089f3bd30de8cc27da993788d31f43ec689c2d4
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-06-16 16:53:22 +0700

    Profile test

--- src/test/java/org/example/ProfileTest.java
+++ src/test/java/org/example/ProfileTest.java
@@ -1,4 +1,60 @@
 package org.example;
 
+import org.example.page.DashboardPage;
+import org.example.page.LoginPage;
+import org.example.page.ProfilePage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class ProfileTest {
+    WebDriver driver;
+    DashboardPage dashboardPage;
+    LoginPage loginPage;
+    ProfilePage profilePage;
+
+    @BeforeEach
+    void setUp() {
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://simklinik.madanateknologi.web.id");
+
+        dashboardPage = new DashboardPage(driver);
+        loginPage = new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
+
+        login();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    void login(){
+        loginPage.fillEmail("kamaluddin.arsyad05@gmail.com");
+        loginPage.fillPassword("admin");
+        loginPage.submitLogin();
+    }
+
+    @Test
+    void updateProfile(){
+        dashboardPage.goToProfile();
+        Assertions.assertTrue(profilePage.isDisplayed());
+        profilePage.goToEdit();
+        String name = "Nama Baru Arsyad";
+        String specialization = "Kucing Gila";
+        profilePage.setName(name);
+        profilePage.setSpecialization(specialization);
+        profilePage.submitEditForm();
+        String newName = profilePage.getName();
+        String newSpecialization = profilePage.getSpecialization();
+        Assertions.assertEquals(name,newName);
+        Assertions.assertEquals(specialization,newSpecialization);
+    }
 }
--- src/test/java/org/example/page/ProfilePage.java
+++ src/test/java/org/example/page/ProfilePage.java
@@ -1,5 +1,6 @@
 package org.example.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProfilePage {
@@ -11,5 +12,31 @@ public class ProfilePage {
 
     public boolean isDisplayed(){
         return driver.getCurrentUrl().endsWith("profile");
+    }
+
+    public  void goToEdit(){
+        driver.findElement(By.xpath("/html/body/div/div[2]/main/div[2]/div[1]/a")).click();
+    }
+
+    public void setName(String name){
+        driver.findElement(By.xpath("//*[@id=\"submitForm\"]//input[@name=\"name\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"submitForm\"]//input[@name=\"name\"]")).sendKeys(name);
+    }
+
+    public void setSpecialization(String specialization){
+        driver.findElement(By.xpath("//*[@id=\"submitForm\"]//input[@name=\"specialization\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"submitForm\"]//input[@name=\"specialization\"]")).sendKeys(specialization);
+    }
+
+    public void submitEditForm(){
+        driver.findElement(By.xpath("//*[@id=\"submitForm\"]")).submit();
+    }
+
+    public String getName(){
+        return driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div[2]/div/p[2]")).getText();
+    }
+
+    public String getSpecialization(){
+        return driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div[2]/div/p[4]")).getText();
     }
 }
