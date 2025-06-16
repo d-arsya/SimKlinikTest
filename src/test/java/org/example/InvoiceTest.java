fb898b9c4b428789ae1316d4f5a34f805cd6fe24
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-06-17 00:29:28 +0700

    invoice

--- src/test/java/org/example/InvoiceTest.java
+++ src/test/java/org/example/InvoiceTest.java
@@ -1,4 +1,87 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.example.page.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class InvoiceTest {
+    WebDriver driver;
+    DashboardPage dashboardPage;
+    LoginPage loginPage;
+    InvoicePage invoicePage;
+    Dotenv dotenv = Dotenv.load();
+
+    @BeforeEach
+    void setUp() {
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get(dotenv.get("SIMKLINIK_URL"));
+
+        dashboardPage = new DashboardPage(driver);
+        loginPage = new LoginPage(driver);
+        invoicePage = new InvoicePage(driver);
+        login();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    void login(){
+        loginPage.fillEmail("super@super.com");
+        loginPage.fillPassword("super");
+        loginPage.submitLogin();
+    }
+
+    @Test
+    void verifyInvoice(){
+        dashboardPage.goToInvoice();
+        Assertions.assertTrue(invoicePage.isDisplayed());
+        String code = invoicePage.getFirstInvoiceCode();
+        invoicePage.goToFirstInvoiceCode();
+        Assertions.assertEquals(code,invoicePage.getInvoiceCode());
+    }
+
+    @Test
+    void addDiscount(){
+        dashboardPage.goToInvoice();
+        Assertions.assertTrue(invoicePage.isDisplayed());
+        invoicePage.goToFirstUnpaidInvoice();
+        String amount = invoicePage.getAmount();
+        invoicePage.addDisc(20);
+        Assertions.assertTrue(invoicePage.getDisc().startsWith("20"));
+        Assertions.assertFalse(invoicePage.getAmount().equals(amount));
+    }
+
+    @Test
+    void addDiscountService(){
+        dashboardPage.goToInvoice();
+        Assertions.assertTrue(invoicePage.isDisplayed());
+        invoicePage.goToFirstUnpaidInvoice();
+        invoicePage.addDiscService();
+        Assertions.assertTrue(invoicePage.getAmount().equals("Rp 0"));
+    }
+
+    @Test
+    void removeDiscount(){
+        dashboardPage.goToInvoice();
+        Assertions.assertTrue(invoicePage.isDisplayed());
+        invoicePage.goToFirstUnpaidInvoice();
+        String amount = invoicePage.getAmount();
+        invoicePage.addDisc(20);
+        Assertions.assertTrue(invoicePage.getDisc().startsWith("20"));
+        Assertions.assertFalse(invoicePage.getAmount().equals(amount));
+
+        invoicePage.removeDisc();
+        Assertions.assertTrue(invoicePage.getDisc().equals("0%"));
+        Assertions.assertTrue(invoicePage.getAmount().equals(amount));
+    }
 }
--- src/test/java/org/example/page/InvoicePage.java
+++ src/test/java/org/example/page/InvoicePage.java
@@ -1,5 +1,6 @@
 package org.example.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class InvoicePage {
@@ -7,5 +8,69 @@ public class InvoicePage {
 
     public InvoicePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public boolean isDisplayed(){
+        return driver.getCurrentUrl().endsWith("invoice");
+    }
+
+    public int getFirstPaidInvoiceCode(){
+        int link = 0;
+        for (int i = 1; i < 10; i++) {
+            String ai = driver.findElement(By.xpath("//*[@id=\"dataTable\"]/tbody/tr["+i+"]/td[9]/a")).getAttribute("href");
+            if (!ai.endsWith("edit")){
+               link = i;
+               break;
+            }
+        }
+        return link;
+    }
+
+    public int getFirstUnpaidInvoiceCode(){
+        int link = 0;
+        for (int i = 1; i < 10; i++) {
+            String ai = driver.findElement(By.xpath("//*[@id=\"dataTable\"]/tbody/tr["+i+"]/td[9]/a")).getAttribute("href");
+            if (ai.endsWith("edit")){
+               link = i;
+               break;
+            }
+        }
+        return link;
+    }
+
+    public String getFirstInvoiceCode(){
+        return driver.findElement(By.xpath("//*[@id=\"dataTable\"]/tbody/tr["+getFirstPaidInvoiceCode()+"]/td[1]")).getText();
+    }
+
+    public void goToFirstInvoiceCode(){
+        driver.findElement(By.xpath("//*[@id=\"dataTable\"]/tbody/tr["+getFirstPaidInvoiceCode()+"]/td[9]/a")).click();
+    }
+
+    public void goToFirstUnpaidInvoice(){
+        driver.findElement(By.xpath("//*[@id=\"dataTable\"]/tbody/tr["+getFirstUnpaidInvoiceCode()+"]/td[9]/a")).click();
+    }
+
+    public String getInvoiceCode(){
+        return driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div[2]/div[2]/div/div/div[2]/p[2]")).getText();
+    }
+
+    public void addDisc(int discount){
+        driver.findElement(By.id("discount-check")).click();
+        driver.findElement(By.id("discount-value")).sendKeys(Integer.toString(discount));
+    }
+
+    public void addDiscService(){
+        driver.findElement(By.id("gratis-pemeriksaan")).click();
+    }
+
+    public void removeDisc(){
+        driver.findElement(By.id("discount-check")).click();
+    }
+
+    public String getDisc(){
+        return driver.findElement(By.id("disc-val")).getText();
+    }
+    public String getAmount(){
+        return driver.findElement(By.id("total-harga")).getText();
     }
 }
